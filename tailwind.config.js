b24818dbcc65f48fcaaa04b423d8a5155d172a55
Andrew Carletti <andrew.carletti25@gmail.com>
2022-09-01 11:45:21 -0300

    Add mobile header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,20 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          600: "#7C3AED",
+        },
+        neutral: {
+          900: "#111827",
+          700: "#374151",
+        },
+      },
+    },
     fontFamily: {
-      sans: ['Inter', 'sans-serif']
-    }
+      sans: ["Inter", "sans-serif"],
+    },
   },
   plugins: [],
 };
