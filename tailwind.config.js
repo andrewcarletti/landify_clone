4f4fc1e6058604fa1e8307ce10e2fce8094c7c57
Andrew Carletti <andrew.carletti25@gmail.com>
2022-09-01 15:18:46 -0300

    add mobile customers stories section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,13 @@ module.exports = {
         primary: {
           600: "#7C3AED",
         },
+        secondary: {
+          100: "#D5FAFC"
+        },
         neutral: {
           900: "#111827",
           700: "#374151",
+          500: "#6B7280",
           200: "#E5E7EB"
         },
       },
